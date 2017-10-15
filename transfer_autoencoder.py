f6bf41e2b66334f571f4817f343e0a7165f1f602
Zhiyu <zhiyushen91@gmail.com>
2017-10-15 13:56:40 +0800

    2017.10.15 13:56

--- transfer_autoencoder.py
+++ transfer_autoencoder.py
@@ -59,8 +59,8 @@ if __name__ == '__main__':
     # Hyper Parameters
     EPOCH = 100
     BATCH_SIZE = 10
-    LR_D = 0.0001
-    LR_G = 0.0001
+    LR_D = 0.0005
+    LR_G = 0.0005
     
     # data
     dataset_size = 100
@@ -86,7 +86,7 @@ if __name__ == '__main__':
     x1 = torch.normal(-20.5*n_data, 1)
     y1 = torch.ones(dataset_size)
     
-    xtn, xtp = x0, x1
+    xtn, xtp = x0[0:5,:], x1[0:5,:]
     xt = torch.cat((x0, x1), 0).type(torch.FloatTensor)
     yt = torch.cat((y0, y1), ).type(torch.LongTensor)
     
@@ -106,23 +106,21 @@ if __name__ == '__main__':
     opt_Dp = torch.optim.Adam(Dp.parameters(), lr=LR_D)
     opt_Dn = torch.optim.Adam(Dn.parameters(), lr=LR_D)
     opt_G = torch.optim.Adam(tsNet.parameters(), lr=LR_G)
-#    for name, param in tsNet.state_dict().items():
-#        if name == 'w':
-#            print(name, param)
-    for step in range(100000):
+    
+    for step in range(10000):
         p_u, p_0, p_1 = tsNet(xt, xtn, xtp)
         
-        prob_s = D(xs)
+        prob_s0 = D(xs)
         prob_t0 = D(p_u)
-        D_loss = -1.* torch.mean(torch.log(prob_s) + torch.log(1. - prob_t0))
+        D_loss = -1.* (torch.mean(torch.log(prob_s0)) + torch.mean(torch.log(1. - prob_t0)))
 
-        prob_s = Dn(xsn)
+        prob_s1 = Dn(xsn)
         prob_t1 = Dn(p_0)
-        Dn_loss = -1.* torch.mean(torch.log(prob_s) + torch.log(1. - prob_t1))
+        Dn_loss = -1.* (torch.mean(torch.log(prob_s1)) + torch.mean(torch.log(1. - prob_t1)))
         
-        prob_s = Dp(xsp)
+        prob_s2 = Dp(xsp)
         prob_t2 = Dp(p_1)
-        Dp_loss = -1.* torch.mean(torch.log(prob_s) + torch.log(1. - prob_t2))
+        Dp_loss = -1.* (torch.mean(torch.log(prob_s2)) + torch.mean(torch.log(1. - prob_t2)))
         
         G_loss = 1.*(torch.mean(torch.log(1. - prob_t0))+torch.mean(torch.log(1. - prob_t1))+torch.mean(torch.log(1. - prob_t2)))
         
@@ -143,68 +141,23 @@ if __name__ == '__main__':
         opt_G.step()
         
         if step % 100 == 0:
-#            print('G_loss:', G_loss.data[0])
             print('step:%d Total_loss:%1.2f'%(step,D_loss.data[0]+Dn_loss.data[0]+Dp_loss.data[0]+G_loss.data[0]))
-            print('D_loss:%1.2f  Dp_loss:%1.2f  Dn_loss:%1.2f  G_loss:%1.2f'%(D_loss.data[0],Dn_loss.data[0],Dp_loss.data[0],G_loss.data[0]))
-#            for name, param in tsNet.state_dict().items():
-#                if name == 'T_pos':
-#                    print(name, param)
-            plt.scatter(xs.data.numpy()[:, 0], xs.data.numpy()[:, 1], c=ys.data.numpy(), s=100, lw=0, cmap='RdYlGn')
+            print('D_loss:%1.2f  Dn_loss:%1.2f  Dp_loss:%1.2f  G_loss:%1.2f'%(D_loss.data[0],Dn_loss.data[0],Dp_loss.data[0],G_loss.data[0]))
+            
+            plt.scatter(xs.data.numpy()[:, 0], xs.data.numpy()[:, 1], c=ys.data.numpy(), s=100, lw=0)
             plt.show()
             
-            plt.scatter(xt.data.numpy()[:, 0], xt.data.numpy()[:, 1], c=yt.data.numpy(), s=100, lw=0, cmap='RdYlGn')
+            plt.scatter(xt.data.numpy()[:, 0], xt.data.numpy()[:, 1], c=yt.data.numpy(), s=100, lw=0)
             plt.show()
     
-#            p_u, p_0, p_1 = tsNet(xt, xtn, xtp)
-            plt.scatter(xs.data.numpy()[:, 0], xs.data.numpy()[:, 1], c=ys.data.numpy(), s=100, lw=0, cmap='RdYlGn')
-            plt.scatter(p_u.data.numpy()[:, 0], p_u.data.numpy()[:, 1])
+#            plt.scatter(xs.data.numpy()[:, 0], xs.data.numpy()[:, 1], c=ys.data.numpy(), s=100, lw=0, cmap='RdYlGn')
+#            plt.scatter(p_u.data.numpy()[:, 0], p_u.data.numpy()[:, 1], c=yt.data.numpy(), s=100, lw=0, cmap='RdYlGn')
+            plt.scatter(np.concatenate((xs.data.numpy()[:, 0], p_u.data.numpy()[:, 0]), 0), 
+                        np.concatenate((xs.data.numpy()[:, 1], p_u.data.numpy()[:, 1]), 0), 
+                        c=np.concatenate((ys.data.numpy(), yt.data.numpy()+2), 0), 
+                        s=100, lw=0)
             plt.show()
-#            print(step,D_loss.data[0],Dn_loss.data[0],Dp_loss.data[0],G_loss.data[0])
 #            time.sleep(1)
-    for name, param in tsNet.state_dict().items():
-        if name == 'w':
-            print(name, param)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#    for name, param in tsNet.state_dict().items():
+#        if name == 'w':
+#            print(name, param)
